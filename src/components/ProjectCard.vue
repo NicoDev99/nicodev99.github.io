21c61012c1c0554039ae161c3afeea916412e158
NicoDev99 <contact.nicopro.dev@gmail.com>
2025-03-04 11:50:34 +0100

    Update

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,7 +2,7 @@
     <div class="max-w-sm lg:max-w-md z-10">
         <a href="/">
             <div class="max-h-80 lg:max-h-96 border-2 border-solid border-white rounded overflow-hidden">
-                <img src="/img/no_project.png" alt="No Project" class="w-full h-full hover:scale-110 duration-200">
+                <img src="/img/no_projects.png" alt="No Project" class="w-full h-full hover:scale-110 duration-200">
             </div>
         </a>
 
