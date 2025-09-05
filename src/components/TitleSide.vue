f499094a4defa581bc369fb39b99921d34fe13d4
NicoDev99 <contact.nicopro.dev@gmail.com>
2025-09-05 11:22:37 +0200

    Create end page

--- src/components/TitleSide.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="flex justify-center items-center">
-        <div class="bg-gradient-to-l from-[#91847F] to-[#2D2B2A]/20 w-12 sm:w-24 h-0.5 hidden xs:block mr-5 sm:mr-10 -translate-x-6" data-aos="slide-right"></div>
-        <slot></slot>
-        <div class="bg-gradient-to-r from-[#91847F] to-[#2D2B2A]/20 w-12 sm:w-24 h-0.5 hidden xs:block ml-5 sm:ml-10 -translate-x-6" data-aos="slide-left"></div>
-    </div>
-</template>
